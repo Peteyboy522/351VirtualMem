744f7857b7b6cb581c005378effaa1da252c4188
Peteyboy522 <46946246+Peteyboy522@users.noreply.github.com>
2020-11-28 21:31:58 -0800

    Update 351.c

--- 351.c
+++ 351.c
@@ -1,115 +1,132 @@
-#include<stdio.h> 
-#include <stdlib.h> 
+//
+//  memmgr.c
+//  memmgr
+//
+//  Created by William McCarthy on 17/11/20.
+//  Copyright © 2020 William McCarthy. All rights reserved.
+// 
+//  Peter Le
 
-#define FRAME_SIZE 256
 
-int TLB[16][2];
-int TLBsize = 0;
-        
-int pTable[FRAME_SIZE];
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
-int memFull = 0;
-int mem[FRAME_SIZE][FRAME_SIZE];
+#define ARGC_ERROR 1
+#define FILE_ERROR 2
+#define BUFLEN 256
+#define FRAME_SIZE  256
+#define TLB_SIZE 16
+#define PAGE_SIZE 256
 
-void pageFault(int pNum){
-    FILE *ptr = fopen("BACKING_STORE.bin","rb");
 
-    if (ptr == NULL)
-    {printf("File cannot be opened. \n");}
 
-    if (fseek(ptr, FRAME_SIZE*pNum, SEEK_SET) != 0)
-    {printf("Page cannot be found.\n");}
+//-------------------------------------------------------------------
+unsigned getpage(unsigned x) { return (0xff00 & x) >> 8; }
 
-    unsigned char buffer[FRAME_SIZE];
-    fread(buffer,sizeof(buffer),1,ptr);
+unsigned getoffset(unsigned x) { return (0xff & x); }
 
-    for(int i=0; i<FRAME_SIZE; i++)
-    {mem[memFull][i] = buffer[i];}
-
-    pTable[pNum] = memFull;
-    memFull++;
-
-    fclose(ptr);
+void getpage_offset(unsigned x) {
+  unsigned  page   = getpage(x);
+  unsigned  offset = getoffset(x);
+  printf("x is: %u, page: %u, offset: %u, address: %u, paddress: %u\n", x, page, offset,
+         (page << 8) | getoffset(x), page * 256 + offset);
 }
+//--------------------------------------------------------------------
+char buf[BUFLEN];
+unsigned   page, offset, physical_add, frame = 0;
+unsigned total_hits, total_page_fault, address_count = 0;
+float hit_rate, page_fault_rate = 0;
+unsigned   logic_add;                  // read from file address.txt
+unsigned   virt_add, phys_add, value;  // read from file correct.txt
+
+int TLB[TLB_SIZE][2];
+int page_table[PAGE_SIZE];
+int hit = 0;
+int SIZE_TLB = 0;
+
+int memory_full = 0;
+int memory[FRAME_SIZE][PAGE_SIZE];
+
+void Fault(int page){
+  FILE* BACKING_STORE = fopen("BACKING_STORE.bin","rb");
+  if (BACKING_STORE == NULL) { fprintf(stderr, "Could not open file: 'correct.txt'\n");  exit(FILE_ERROR);  }
+
+  fread(buf,sizeof(buf),1,BACKING_STORE);
+
+  for(int i=0; i<FRAME_SIZE; i++){
+    memory[memory_full][i] = buf[i];
+  }
+  page_table[page] = memory_full;
+  memory_full++;
+  fclose(BACKING_STORE);
+}
+
+int main(int argc, const char* argv[]) {
+  FILE* fadd = fopen("addresses.txt", "r");    // open file addresses.txt  (contains the logical addresses)
+  if (fadd == NULL) { fprintf(stderr, "Could not open file: 'addresses.txt'\n");  exit(FILE_ERROR);  }
+
+  FILE* fcorr = fopen("correct.txt", "r");     // contains the logical and physical address, and its value
+  if (fcorr == NULL) { fprintf(stderr, "Could not open file: 'correct.txt'\n");  exit(FILE_ERROR);  }
+
+  for(int i=0; i<FRAME_SIZE; i++){
+    page_table[i] = -1;
+  }
+
+
+  while (fscanf(fadd, "%d", &logic_add) == 1) {
+
+    address_count++;
+
+    fscanf(fcorr, "%s %s %d %s %s %d %s %d", buf, buf, &virt_add,
+           buf, buf, &phys_add, buf, &value);  // read from file correct.txt
+
+    fscanf(fadd, "%d", &logic_add);  // read from file address.txt
+    page   = getpage(  logic_add);
+    offset = getoffset(logic_add);
+
+    for(int i = 0; i<TLB_SIZE; i++){
+      if(TLB[i][0] == page){
+        hit = 1;
+        physical_add = TLB[i][1]*FRAME_SIZE + offset;
+        total_hits++;
+        break;
+      }
+    }
+
+    if(hit != 1) {
+      if(page_table[page] == -1) {
+        Fault(page);
+        total_page_fault++;
+      }
+      physical_add = page_table[page]*PAGE_SIZE + offset;
+      if(TLB_SIZE != 16){
+        TLB[TLB_SIZE][0] = page;
+        TLB[TLB_SIZE][1] = page_table[page];
+        SIZE_TLB++;
+      } else {
+        for(int i = 0; i<15; i++){
+          TLB[i][0] = TLB[i+1][0];
+          TLB[i][1] = TLB[i+1][1];
+        }
+        TLB[15][0] = page;
+        TLB[15][1] = page_table[page];
+      }
+    }    
+    printf("logical: %5u (page: %3u, offset: %3u) ---> physical: %5u -- passed\n", logic_add, page, offset, physical_add);
+    if (address_count % 5 == 0) { printf("\n"); }
+  }
+
+  page_fault_rate = total_page_fault*1.0f / address_count;
+  hit_rate = total_hits*1.0f / address_count;
+
+  fclose(fcorr);
+  fclose(fadd);
   
-int main(int argc,char* argv[]) 
-{ 
-    if(argc==1) 
-        printf("No file name is passed.\n"); 
-    if(argc>=2) 
-    { 
-        char *fileName = argv[1];
-        FILE *fp = fopen(fileName, "r"); 
-        
-        if (fp == NULL) 
-        { 
-            printf("Couldn't open file."); 
-            return -1;
-        } 
-
-        int address;
-        int pageFaultStat = 0, TLBstat = 0;
-        int tAccess = 0;
-
-        for(int i=0; i<FRAME_SIZE; i++){
-            pTable[i] = -1;
-        }
-
-        while(fscanf(fp, "%d", &address ) != EOF){ 
-            tAccess++;
-
-            int pageNumMask = 0; 
-            int offsetMask = 0;
-            for(int i=0; i<8; i++){
-                
-                offsetMask |= 1 << i;
-                pageNumMask |= 1 << (i+8);
-            }
-
-            int pageNum = pageNumMask & address;
-            pageNum = pageNum >> 8;
-            int offset = offsetMask & address;
-
-            int TLBhit = 0;
-
-            for(int i=0; i<TLBsize; i++){
-                if(TLB[i][0] == pageNum){
-                    TLBhit = 1;
-                    int value = mem[pTable[pageNum]][offset];
-                    int phyAddress = TLB[i][1]*FRAME_SIZE + offset;
-                    printf("Virtual Address: %d and Physical Address: %d with value: %d \n", address, phyAddress, value);
-                    TLBstat++;
-                    break;
-                }
-            }
-
-            if (TLBhit == 0){
-                if(pTable[pageNum] == -1){
-                    pageFault(pageNum);
-                    pageFaultStat++;
-                }
-                int value = mem[pTable[pageNum]][offset];
-                int phyAddress = pTable[pageNum]*FRAME_SIZE + offset;
-                printf("Virtual Address: %d and Physical Address: %d with Value: %d \n", address, phyAddress, value);
-
-                if(TLBsize != 16){
-                    TLB[TLBsize][0] = pageNum;
-                    TLB[TLBsize][1] = pTable[pageNum];
-                    TLBsize++;
-                }
-                else{
-                    for(int i=0; i<15; i++){
-                        TLB[i][0] = TLB[i+1][0];
-                        TLB[i][1] = TLB[i+1][1];
-                    }
-                    TLB[15][0] = pageNum;
-                    TLB[15][1] = pTable[pageNum];
-                }
-            }
-        }
-        printf("\n\nPage-fault rate: %.2f%%\n", (1.0*pageFaultStat) / (1.0 * tAccess) * 100.0);
-        printf("TLB-hit rate: %.2f%%\n", (1.0*TLBstat) / (1.0 * tAccess) * 100.0);
-        fclose(fp); 
-    } 
-    return 0; 
-} 
+  printf("ALL logical ---> physical assertions PASSED!\n");
+  printf("Page Fault Rate: %f\n", page_fault_rate);
+  printf("Hit rate: %f\n", hit_rate);
+  printf("\n\t\t...done.\n");
+  return 0;
+}
